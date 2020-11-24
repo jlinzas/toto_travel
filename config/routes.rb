1c730a8993fc85d8c3d6a38cf4d6244ed5fc4eab
Julien Linzas <linzas.julien@gmail.com>
2020-11-24 13:21:00 +0100

    creation de cloudinary et des controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'spaceships/index'
+  get 'spaceships/new'
+  get 'spaceships/show'
+  get 'spaceships/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
