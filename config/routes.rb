c2d8354b7babb8a41ad19fd3572dac93f04a3e1c
Julien Linzas <linzas.julien@gmail.com>
2020-11-25 12:02:17 +0100

    reajustement des routes pour le pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 resources :spaceships
+resources :users, only: [:show] do
+  resources :orders, only: [:new, :create]
+end
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(email: "Bernard@example.com", encrypted_password: "#$taawktljasktlw4aaglj")
+# User.create(email: "Stephane@example.com", encrypted_password: "#$taawktljasktlw4aaglj")
+# User.create(email: "Gilou@example.com", encrypted_password: "#$taawktljasktlw4aaglj")
+# User.create(email: "Sophie@example.com", encrypted_password: "#$taawktljasktlw4aaglj")
+
+Spaceship.create(name: "Dogo", speed: "350 000", number_of_seats: "6", price: 350, user_id: 1)
+Spaceship.create(name: "Marcel", speed: "20", number_of_seats: "2", price: 100, user_id: 1)
+Spaceship.create(name: "Leon", speed: "45", number_of_seats: "3", price: 120, user_id: 1)
+Spaceship.create(name: "Remi", speed: "120", number_of_seats: "5", price: 200, user_id: 1)
+Spaceship.create(name: "Le routier", speed: "50 000", number_of_seats: "6", price: 180, user_id: 1)
