f1907f48bc719643367fc62c37eb4a5310bfbc54
Alexandre Dosseto <leonedimars@gmail.com>
2020-11-26 18:15:04 +0100

    back research name and price index

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -3,4 +3,18 @@ class Spaceship < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_one_attached :photo
   validates :name, :number_of_seats, :speed, :price, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [ :name, :price],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
+
+
+    # t.string "name"
+    # t.text "description"
+    # t.integer "number_of_seats"
+    # t.string "speed"
+    # t.integer "price"
